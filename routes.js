c31221fc3e94df6df6f1dcf13953a1a15623d7ab
k.ayushbatwada <ayush.batwara@krayonnz.com>
2024-08-23 18:03:03 +0530

    added model and routes changes for posts

--- routes.js
+++ routes.js
@@ -1,5 +1,8 @@
-const healthCheck = require('./server/utils/healthCheck')
+const healthCheck = require('./server/utils/healthCheck');
+const postRouter = require('./server/posts/routes');
 
 module.exports = (app) => {
-    app.use('/v1/api/healthCheck', healthCheck);
+    app.use('/healthCheck', healthCheck);
+
+    app.use('v1/api/post', postRouter);
 }
